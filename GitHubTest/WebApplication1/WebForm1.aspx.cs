b2ae1fc53bb9b5837b7af3d3f828ad92ea2f3760
hakanmenekse <hakanmenekse@outlook.com>
2016-12-12 15:19:12 +0300

    3333333

--- GitHubTest/WebApplication1/WebForm1.aspx.cs
+++ GitHubTest/WebApplication1/WebForm1.aspx.cs
@@ -30,6 +30,7 @@ namespace WebApplication1
                 e.Editor.ReadOnly = true;
                 e.Editor.ClientEnabled = false;
                 //test_2...
+                //test3...
             }
 
         }
